db61aa742869ae4b0dbccb5f1c87b07ad7c9bf57
Hayden <hmorran@outlook.com>
2019-03-13 19:37:54 +1100

    Updates

---
+++ precache-manifest.5c1d178d5f1fe038b8251f29a87081c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "c92e4921892e5a1b6a75",
+    "url": "/robofriends/static/js/main.285a0bb8.chunk.js"
+  },
+  {
+    "revision": "3e11b5c15bbe70cb2235",
+    "url": "/robofriends/static/js/2.845af92a.chunk.js"
+  },
+  {
+    "revision": "c92e4921892e5a1b6a75",
+    "url": "/robofriends/static/css/main.fa33556e.chunk.css"
+  },
+  {
+    "revision": "3e11b5c15bbe70cb2235",
+    "url": "/robofriends/static/css/2.6cff7bcb.chunk.css"
+  },
+  {
+    "revision": "22b3372cfe4de2827fb7c56d70346077",
+    "url": "/robofriends/index.html"
+  }
+];
